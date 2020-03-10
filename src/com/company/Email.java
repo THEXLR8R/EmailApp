62198b6ea66573fbe156f724b7c13043b38a5c89
THEXLR8R <tihomir.bogdanov.2003@gmail.com>
2020-03-10 17:20:32 +0200

    New project - Email App

---
+++ src/com/company/Email.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Email {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailBoxCapacity;
+    private String alternateEmail;
+
+   // const 2 recive first and last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email created : " + this.firstName + " " + this.lastName);
+    }
+
+    // ask for department
+
+    // generate a random password
+
+    // set the capacity for mailbox
+
+    // set the alternative mail
+
+    // change the password
+
+
+}
---
+++ src/com/company/EmailApp.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class EmailApp {
+
+    public static void main(String[] args) {
+
+        Email eml1 = new Email("Alice","Cooper");
+    }
+}
